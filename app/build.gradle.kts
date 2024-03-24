53153e11cb816cf3f568a8abba5be3e589b83058
Charles Martin <cpascual185@gmail.com>
2024-03-24 12:35:23 +0800

    Build 26: Room database.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0 build 25"
+        versionName = "1.0 build 26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/phroton/notes/NoteViewModel.java
+++ app/src/main/java/com/phroton/notes/NoteViewModel.java
@@ -4,10 +4,25 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
 
 public class NoteViewModel extends AndroidViewModel {
-
+    private NoteRepository mRepository;
+    private LiveData<List<Note>> mNotes;
     public NoteViewModel(@NonNull Application application) {
         super(application);
+
+        mRepository = new NoteRepository(application);
+        mNotes = mRepository.getNotes();
+    }
+
+    LiveData<List<Note>> getNotes(){
+        return mNotes;
+    }
+
+    public void insert(Note note){
+        mRepository.insert(note);
     }
 }
