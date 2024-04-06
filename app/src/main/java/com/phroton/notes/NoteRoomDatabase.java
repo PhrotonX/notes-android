edfad41ec59000b16d3f5ee8a5e70e0fbe21956a
Charles Martin <cpascual185@gmail.com>
2024-04-06 19:38:42 +0800

    Build 37: Add Sample Data on new installs

--- app/src/main/java/com/phroton/notes/NoteRoomDatabase.java
+++ app/src/main/java/com/phroton/notes/NoteRoomDatabase.java
@@ -38,6 +38,20 @@ public abstract class NoteRoomDatabase extends RoomDatabase{
 
     }
 
+    private void InflateSampleData(){
+        noteDao().insert(new Note("Sample Data 1", "The quick brown fox jumps over" +
+                " the lazy dog"));
+        noteDao().insert(new Note("Sample Data 2", "Lorem ipsum dolor sit amet," +
+                " consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " +
+                "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
+                "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+                "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
+                "deserunt mollit anim id est laborum."));
+        noteDao().insert(new Note("Sample Data 3", "Take notes by tapping the + " +
+                "button."));
+    }
+
     @NonNull
     @Override
     protected InvalidationTracker createInvalidationTracker() {
