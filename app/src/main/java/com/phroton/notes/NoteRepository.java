226804fa90ad621193599eeab284891a40b815a7
Charles Martin <cpascual185@gmail.com>
2024-04-28 14:32:00 +0800

    Build 77: Fix Database Insert.

--- app/src/main/java/com/phroton/notes/NoteRepository.java
+++ app/src/main/java/com/phroton/notes/NoteRepository.java
@@ -13,67 +13,26 @@ public class NoteRepository {
     private NoteDao mNoteDao;
     private LiveData<List<Note>> mNotes;
 
-    /*interface RepositoryCallback<T>{
-        void onComplete(Result<T> result);
-    }*/
-
     NoteRepository(Application application){
         NoteRoomDatabase roomDatabase = NoteRoomDatabase.getDatabase(application);
         mNoteDao = roomDatabase.noteDao();
 
-        NoteRoomDatabase.databaseWriteExecutor.execute(() -> {
-            mNotes = mNoteDao.getAllNotes();
-        });
+        mNotes = mNoteDao.getAllNotes();
     }
 
     public LiveData<List<Note>> getNotesCompat(){
         return mNotes;
     }
-    /*public void getNotes(final RepositoryCallback<LiveData<List<Note>>> callback){
-        try {
-            NoteRoomDatabase.databaseWriteExecutor.execute(new Runnable() {
-                @Override
-                public void run(){
-                    try{
-                        Result<LiveData<List<Note>>> result = getNotesFromDatabase();
-                        callback.onComplete(result);
-                    }catch(Exception e){
-                        e.printStackTrace();
-                        Result<LiveData<List<Note>>> errorResult = getNotesFromDatabase();
-                        callback.onComplete(errorResult);
-                    }
-                }
-            });
-
-            //return mNoteDao.getNotesByDescendingId();
-
-        }catch(Exception e){
-            Log.d("com.phroton.notes", "Failed to retreive data from RoomDatabase");
-            e.printStackTrace();
-        }
-
-    }*/
-
-    /*private Result<LiveData<List<Note>>> getNotesFromDatabase(){
-        try{
-            return new Result.Success<LiveData<List<Note>>>(mNoteDao.getNotesByDescendingId());
-        }catch(Exception e){
-            return new Result.Error<LiveData<List<Note>>>(e);
-        }
-    }*/
 
     public void insert(Note note){
-        /*try{
+        try{
             NoteRoomDatabase.databaseWriteExecutor.execute(() -> {
                 mNoteDao.insert(note);
             });
         }catch(NullPointerException e){
             Log.e("com.phroton.notes", "NoteDao is null!");
             e.printStackTrace();
-        }*/
-        NoteRoomDatabase.databaseWriteExecutor.execute(() -> {
-            mNoteDao.insert(note);
-        });
+        }
     }
 
 }
--- app/src/main/java/com/phroton/notes/NoteViewModel.java
+++ app/src/main/java/com/phroton/notes/NoteViewModel.java
@@ -19,21 +19,6 @@ public class NoteViewModel extends AndroidViewModel {
         mNotes = mRepository.getNotesCompat();
     }
 
-    /*public LiveData<List<Note>> getNotes(){
-        mRepository.getNotes(new NoteRepository.RepositoryCallback<LiveData<List<Note>>>(){
-            @Override
-            public void onComplete(Result<LiveData<List<Note>>> result){
-                if(result instanceof Result.Success){
-                    mNotes = ((Result.Success<LiveData<List<Note>>>) result).data;
-                }else{
-                    ((Result.Error<LiveData<List<Note>>>) result).exception.printStackTrace();
-                }
-            }
-        });
-
-        return mNotes;
-    }*/
-
     public LiveData<List<Note>> getNotesCompat(){
 
         return mNotes;
