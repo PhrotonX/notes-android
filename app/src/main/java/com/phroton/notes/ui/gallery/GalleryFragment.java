93d7bd5b719523c7cff458eaac8f736dabe11356
Charles Martin <cpascual185@gmail.com>
2024-05-17 21:41:24 +0800

    Build 95: Fix ClickListener

--- app/src/main/java/com/phroton/notes/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/phroton/notes/ui/gallery/GalleryFragment.java
@@ -1,31 +1,52 @@
 package com.phroton.notes.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.phroton.notes.NoteViewAdapter;
 import com.phroton.notes.databinding.FragmentGalleryBinding;
+import com.phroton.notes.ui.editor.EditorActivity;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
+    private NoteViewAdapter mNoteViewAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
+        /*GalleryViewModel galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
+
+
+
+        final TextView textView = binding.textGallery;
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        RecyclerView notesView = binding.galleryList;
+        notesView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        mNoteViewAdapter = new NoteViewAdapter(requireContext());
+        mNoteViewAdapter.setOnClickListener(new NoteViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(requireContext(), "Sample Click Message", Toast.LENGTH_SHORT).show();
+            }
+        });
+        notesView.setAdapter(mNoteViewAdapter);
+        mNoteViewAdapter.notifyDataSetChanged();
         return root;
     }
 
