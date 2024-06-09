3869a1d37be76843bbd88909b9b5747f06bab16e
Charles Martin <cpascual185@gmail.com>
2024-06-09 22:33:44 +0800

    Build 114: Update Database Version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.phroton.notes"
         minSdk = 24
         targetSdk = 34
-        versionCode = 103
-        versionName = "v0.1.0.1 build 113"
+        versionCode = 104
+        versionName = "v0.1.0.1 build 114"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
