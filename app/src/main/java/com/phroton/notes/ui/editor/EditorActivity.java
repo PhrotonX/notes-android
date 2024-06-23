6cc356e96549f0e380669720985f7b68e59c32a1
Charles Martin <cpascual185@gmail.com>
2024-06-23 15:57:52 +0800

    Build 163: Fix item position-related issues.

--- app/src/main/java/com/phroton/notes/ui/editor/EditorActivity.java
+++ app/src/main/java/com/phroton/notes/ui/editor/EditorActivity.java
@@ -109,7 +109,7 @@ public class EditorActivity extends AppCompatActivity {
                         mNoteViewModel.getNotesCompat().observe(this, new Observer<List<Note>>() {
                             @Override
                             public void onChanged(List<Note> notes) {
-                                Note note = notes.get(mPosition - 1);
+                                Note note = notes.get(mPosition);
                                 mEditorTitle.setText(note.getTitle());
                                 mEditorContent.setText(note.getContent());
                                 ChangeBackgroundColor(note.getColor());
