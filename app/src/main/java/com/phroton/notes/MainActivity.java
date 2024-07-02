bfb00a45ab36f7e38ee3c4e5a56b11470770362f
Charles Martin <cpascual185@gmail.com>
2024-07-02 16:31:16 +0800

    Build 190: Fix MainACtivity.

--- app/src/main/java/com/phroton/notes/MainActivity.java
+++ app/src/main/java/com/phroton/notes/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         mNoteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
 
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         }*/
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
+        setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
