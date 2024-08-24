43c8ebb8d220b4bade00f9e051d6929af6f49451
Charles Martin <cpascual185@gmail.com>
2024-08-24 11:41:56 +0800

    Build 260: Fix toolbar issues in Search UI

---
+++ app/src/main/java/com/phroton/notes/ui/search/SearchActivity.java
@@ -0,0 +1,113 @@
+package com.phroton.notes.ui.search;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.phroton.notes.R;
+import com.phroton.notes.databinding.ActivityMainBinding;
+
+/*
+    @TODO: Make this class inherit MainActivity.
+ */
+public class SearchActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ConstraintLayout mLayout;
+    private NavController mNavController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mLayout = (ConstraintLayout)findViewById(R.id.mainLayout);
+
+        setUpToolbar();
+
+        handleIntent(getIntent());
+        setUpFragment(savedInstanceState);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+
+        handleIntent(getIntent());
+    }
+
+    private void handleIntent(Intent intent){
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(getApplicationContext(), "Query: " + query, Toast.LENGTH_SHORT).show();
+            //function for searching into the DB.
+
+            setResultLabel(query);
+        }
+
+    }
+
+    private void setResultLabel(String query){
+        TextView searchLabel = (TextView)findViewById(R.id.infoText);
+        searchLabel.setVisibility(View.VISIBLE);
+        String searchResult = getResources().getString(R.string.results_for) + query;
+        searchLabel.setText(searchResult);
+    }
+
+    private void setUpFragment(Bundle savedInstanceState){
+        NavHostFragment fragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_main);
+        assert fragment != null;
+        mNavController = fragment.getNavController();
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(mNavController.getGraph())
+                .setOpenableLayout(drawer)
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, mNavController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, mNavController);
+
+        if(savedInstanceState == null){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.nav_host_fragment_content_main, new SearchFragment());
+            fragmentTransaction.commit();
+        }
+    }
+
+    private void setUpToolbar(){
+        setSupportActionBar(binding.appBarMain.toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
