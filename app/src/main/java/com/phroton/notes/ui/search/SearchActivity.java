003d8738fe803bda960a1d1e214896d7d4c4a746
Charles Martin <cpascual185@gmail.com>
2024-08-24 11:50:54 +0800

    Build 261: Fix toolbar issues in Search UI

--- app/src/main/java/com/phroton/notes/ui/search/SearchActivity.java
+++ app/src/main/java/com/phroton/notes/ui/search/SearchActivity.java
@@ -4,6 +4,7 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -74,6 +75,15 @@ public class SearchActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void setResultLabel(String query){
         TextView searchLabel = (TextView)findViewById(R.id.infoText);
         searchLabel.setVisibility(View.VISIBLE);
@@ -108,6 +118,7 @@ public class SearchActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_back_dark);
         }
     }
 }
