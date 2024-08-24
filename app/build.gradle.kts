f4030d9f78a3e8ce9d38896b5c76d6c6c54a5ae3
Charles Martin <cpascual185@gmail.com>
2024-08-24 11:30:34 +0800

    Build 259: Fix crash in Search UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 108
-        versionName = "v0.1.0.1 build 258"
+        versionName = "v0.1.0.1 build 259"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
