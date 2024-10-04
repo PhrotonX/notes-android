f5478932868fbaa437550262f08b2f070077a063
Charles Martin <cpascual185@gmail.com>
2024-10-04 22:03:39 +0800

    Build 289: Re-enable some debug messages in EditorActivity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 109
-        versionName = "v0.1.0.3 build 288"
+        versionName = "v0.1.0.3 build 289"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/phroton/notes/ui/editor/EditorActivity.java
+++ app/src/main/java/com/phroton/notes/ui/editor/EditorActivity.java
@@ -110,8 +110,8 @@ public class EditorActivity extends AppCompatActivity {
                 case REQUEST_CODE_EDIT_NOTE:
                     mDbPosition = intent.getIntExtra(Note.NOTE_ID_EXTRA, -1);
                     mRvPosition = intent.getIntExtra(Note.NOTE_POSITION_EXTRA, -1);
-                    //Toast.makeText(this, "EditorAcitvity DbPosition: " + mDbPosition, Toast.LENGTH_SHORT).show();
-                    //Toast.makeText(this, "EditorAcitvity RvPosition: " + mRvPosition, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "EditorAcitvity DbPosition: " + mDbPosition, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "EditorAcitvity RvPosition: " + mRvPosition, Toast.LENGTH_SHORT).show();
 
                     if(mDbPosition != -1){
                         mNoteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
