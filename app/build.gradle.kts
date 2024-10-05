2e5573d5cc799abba7ec89ad879e5b4080ae0e46
Charles Martin <cpascual185@gmail.com>
2024-10-05 21:46:24 +0800

    Build 296: Fix note removal issues.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 109
-        versionName = "v0.1.0.3 build 295"
+        versionName = "v0.1.0.3 build 296"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
