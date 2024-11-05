5da6f52f07a4c014d7bc49140978f5e949a7f1f2
Charles Martin <cpascual185@gmail.com>
2024-11-05 20:46:07 +0800

    Add Kotlin file for TaggedNote

--- app/src/main/java/TaggedNote.kt
+++ app/src/main/java/TaggedNote.kt
@@ -1,2 +1,10 @@
-class TaggedNote {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+@Entity(primaryKeys = ["note_id", "tag_id"])
+data class TaggedNote (
+    @ColumnInfo(name = "note_id")
+    var noteId: Long,
+    @ColumnInfo(name = "tag_id")
+    var tagId: Long
+)
