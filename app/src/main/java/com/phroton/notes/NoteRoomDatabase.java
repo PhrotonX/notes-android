a65f6f10a8e0b9d900e6c0b2a8ff442042a6b7eb
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-11-10 17:43:54 +0800

    Update TaggedNoteRepository

--- app/src/main/java/com/phroton/notes/NoteRoomDatabase.java
+++ app/src/main/java/com/phroton/notes/NoteRoomDatabase.java
@@ -14,7 +14,7 @@ import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import androidx.sqlite.db.SupportSQLiteOpenHelper;
 
-import com.phroton.notes.data.taggednote.TaggedNote;
+import com.phroton.notes.data.taggednote.TaggedNoteDao;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -30,13 +30,13 @@ public abstract class NoteRoomDatabase extends RoomDatabase{
 
     public abstract NoteDao noteDao();
     public abstract TagDao tagDao();
-    public abstract TaggedNote taggedNoteDao();
+    public abstract TaggedNoteDao taggedNoteDao();
     private static volatile NoteRoomDatabase INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
     static ExecutorService databaseWriteExecutor =
             Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
-    static NoteRoomDatabase getDatabase(final Context context){
+    public static NoteRoomDatabase getDatabase(final Context context){
         if(INSTANCE == null){
             synchronized (NoteRoomDatabase.class){
                 if(INSTANCE == null){
