f55febe99229306ef1e566371e32241e6f43f3b7
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-11-10 19:11:00 +0800

    Add PrimaryKey annotation to TaggedNote.kt

--- app/src/main/java/com/phroton/notes/data/taggednote/TaggedNote.kt
+++ app/src/main/java/com/phroton/notes/data/taggednote/TaggedNote.kt
@@ -2,11 +2,15 @@ package com.phroton.notes.data.taggednote
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "tagged_note", primaryKeys = ["note_id", "tag_id"])
 data class TaggedNote (
+    @PrimaryKey
     @ColumnInfo(name = "note_id")
     var noteId: Long,
+
+    @PrimaryKey
     @ColumnInfo(name = "tag_id")
     var tagId: Long
 )
