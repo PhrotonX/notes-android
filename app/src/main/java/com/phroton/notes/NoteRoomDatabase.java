9a3fbf84ac3b8275ee4e5e99d618987c5c7f054c
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-11-10 19:11:43 +0800

    Update NoteRoomDatabase

--- app/src/main/java/com/phroton/notes/NoteRoomDatabase.java
+++ app/src/main/java/com/phroton/notes/NoteRoomDatabase.java
@@ -19,7 +19,7 @@ import com.phroton.notes.data.taggednote.TaggedNoteDao;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {Note.class}, version = 112, exportSchema = true/*,
+@Database(entities = {Note.class, Tag.class, TaggedNoteDao.class}, version = 112, exportSchema = true/*,
     autoMigrations = {
         @AutoMigration(from = 106, to = 108),
             @AutoMigration(from = 107, to = 108)
