322a5b381f536e44d484eca43cfa78f8840684b3
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2024-12-01 20:08:39 +0800

    Build 308: Replace TagListActivity with TagListFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 112
-        versionName = "v0.1.0.4 build 307"
+        versionName = "v0.1.0.4 build 308"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
---
+++ app/src/main/java/com/phroton/notes/ui/tags/TagListFragment.java
@@ -0,0 +1,74 @@
+package com.phroton.notes.ui.tags;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.phroton.notes.R;
+import com.phroton.notes.Tag;
+import com.phroton.notes.TagViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TagListFragment extends Fragment {
+
+    private RecyclerView mTagRecyclerView;
+    private TagViewModel mTagViewModel;
+    public TagListFragment() {
+        // Required empty public constructor
+    }
+
+    public TagViewModel getViewModel(){
+        return mTagViewModel;
+    }
+
+    public RecyclerView getRecyclerView(){
+        return mTagRecyclerView;
+    }
+
+    public void setRecyclerView(RecyclerView recyclerView){
+        mTagRecyclerView = recyclerView;
+    }
+
+    public void setViewModel(AndroidViewModel viewModel){
+        mTagViewModel = (TagViewModel) viewModel;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         mTagViewModel = new ViewModelProvider(this).get(TagViewModel.class);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_tag_list, container, false);
+        getViewModel().getTags().observe(getViewLifecycleOwner(), new Observer<List<Tag>>() {
+            @Override
+            public void onChanged(List<Tag> tags) {
+                TagListAdapter tagListAdapter = new TagListAdapter(getContext(), new ArrayList<>(tags));
+                setRecyclerView((RecyclerView) root.findViewById(R.id.tag_list));
+                getRecyclerView().setLayoutManager(new GridLayoutManager(getContext(),
+                        GridLayoutManager.DEFAULT_SPAN_COUNT, GridLayoutManager.VERTICAL, false));
+            }
+        });
+
+        // Inflate the layout for this fragment
+        return root;
+
+    }
+}
