216dc9a84e97556ce7f2ce40d8c17f36fd0ba60d
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-01-05 16:16:54 +0800

    Build 338: Update TagFragment layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 113
-        versionName = "v0.1.0.4 build 337"
+        versionName = "v0.1.0.4 build 338"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
