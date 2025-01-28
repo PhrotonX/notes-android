47be74b0d7c69ba4948fdd112aa57d32b6a8270e
Charles Martin <59857004+PhrotonX@users.noreply.github.com>
2025-01-28 16:33:58 +0800

    Add foreign keys to TaggedNote

--- app/src/main/java/com/phroton/notes/data/taggednote/TaggedNote.kt
+++ app/src/main/java/com/phroton/notes/data/taggednote/TaggedNote.kt
@@ -2,9 +2,23 @@ package com.phroton.notes.data.taggednote
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.phroton.notes.Note
+import com.phroton.notes.Tag
 
-@Entity(tableName = "tagged_note", primaryKeys = ["note_id", "tag_id"])
+@Entity(tableName = "tagged_note", primaryKeys = ["note_id", "tag_id"],
+    foreignKeys = [
+        ForeignKey(entity = Note::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("noteId"),
+            onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Tag::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("tagId"),
+            onDelete = ForeignKey.CASCADE)
+    ]
+)
 data class TaggedNote (
     @ColumnInfo(name = "note_id")
     var noteId: Long = 0L,
